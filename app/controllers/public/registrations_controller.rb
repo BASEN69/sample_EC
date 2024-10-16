cc989a1ce482764463fe760b9bd6eec75d248dfb
TakuroSemba <171462421+BASEN69@users.noreply.github.com>
2024-10-16 03:26:55 +0000

    管理者・顧客のログイン・ログアウト　次回顧客の登録画面作成

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -49,6 +49,11 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # def configure_account_update_params
   #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
   # end
+  
+  # If you have extra params to permit, append them to the sanitizer.
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:name])
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Admin.find_or_create_by!(email: "admin@exmple.com") do |admin|
+  admin.password = "asasas1969"
+end
+
+Customer.find_or_create_by!(email: "as@exmple.com") do |customer|
+  customer.password = "asasas1919"
+  customer.first_name = "川越"
+  customer.last_name = "太郎"
+  customer.first_name_kana = "カワゴエ"
+  customer.last_name_kana = "タロウ"
+  customer.postal_code = "3501111"
+  customer.address = "埼玉県川越市"
+  customer.telephone_number = "00000000000"
+end
