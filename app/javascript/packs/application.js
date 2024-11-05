94860a739ef206e93775c0ec3909d7522ce76455
TakuroSemba <171462421+BASEN69@users.noreply.github.com>
2024-11-05 09:24:18 +0000

    README更新

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,11 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "jquery"
+import "script.js"
+import Swiper from 'swiper/swiper-bundle.js';
+import 'swiper/swiper-bundle.css'
 
-import "jquery";
 import "popper.js";
 import "bootstrap";
 import "../stylesheets/application";
