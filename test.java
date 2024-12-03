6064ce2bf7dc490ee2fcf59066232850952ba4d4
TakuroSemba <171462421+BASEN69@users.noreply.github.com>
2024-12-03 08:52:00 +0000

    java

--- test.java
+++ test.java
@@ -1,7 +1,43 @@
 class Test {
+  
+  
   public static void main(String[] args) {
-    System.out.println("Hello");
-
-    System.out.println(12/4 == 3);
+    printData(fullName("Kate", "Jones"), 27, 1.6, 50.0);
+    printData(fullName("John", "Christopher", "Smith"), 65, 1.75, 80.0);
   }
+
+  public static void printData(String name, int age, double height, double weight) {
+    System.out.println("私の名前は" + name + "です");
+    System.out.println("年齢は" + age + "歳です");
+    System.out.println("身長は" + height + "mです");
+    System.out.println("体重は" + weight + "kgです");
+    double bmi = bmi(height, weight);
+    System.out.println("BMIは" + bmi + "です");
+    
+    // isHealthyメソッドの結果
+    if (isHealthy(bmi)) {
+      System.out.println("健康です");
+    } else {
+      System.out.println("健康ではありません");
+    }
+    
+  }
+
+  public static String fullName(String firstName, String lastName) {
+    return firstName + " " + lastName;
+  }
+
+  public static String fullName(String firstName, String middleName, String lastName) {
+    return firstName + " " + middleName + " " + lastName;
+  }
+
+  public static double bmi(double height, double weight) {
+    return weight / height / height;
+  }
+
+  // isHealthyメソッド
+  public static boolean isHealthy(double bmi) {
+    return 18.5 <= bmi && bmi < 25.0; 
+  }
+  
 }
